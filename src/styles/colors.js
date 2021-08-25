ba9f05c7e2ad43e45b7b7f5876620c4bb0461e2d
taylorbyks <taylorbyks@hotmail.com>
2021-08-25 16:30:07 -0300

    Add secundary

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,10 +1,10 @@
 export const colors = {
-  primary: '#0052b0',
+  primary: '#0098d1',
+  secundary: '#0052b0',
   title: '#12315b',
   text: '#000',
   red: '#ff4c4c',
   backgroundLight: '#00e3f7',
-  backgroundDark: '#0098d1',
   grey: '#E5E5E5',
   white: '#FFFFFF',
 }
