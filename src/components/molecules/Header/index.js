0ea4283fc1ab657b288a55510ec373bd4d8f9065
taylorbyks <taylorbyks@hotmail.com>
2021-08-25 16:30:15 -0300

    Add options at header

--- src/components/molecules/Header/index.js
+++ src/components/molecules/Header/index.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import { HeaderContainer, HeaderRightContainer, HeaderLeftContainer } from './styles'
 import { Title } from '../../atoms'
+import { useNavigation } from '@react-navigation/native'
 
 import Icon from 'react-native-vector-icons/Ionicons'
 import { Button } from 'react-native'
 
-export const Header = ({ title, children }) => {
+export const Header = ({ title, children, goBack, goBackDisable = false }) => {
+  const navigation = useNavigation()
+
+  function handleGoBack() {
+    console.log(goBack)
+    navigation.navigate(goBack)
+  }
+
   return (
     <HeaderContainer>
       <HeaderLeftContainer>
-        <Button title="< Voltar" />
+        {
+          !goBackDisable &&
+          <Button title="< Voltar" onPress={handleGoBack}/>
+        }
         <Title>{title}</Title>
       </HeaderLeftContainer>
       <HeaderRightContainer>{children}</HeaderRightContainer>
