a7c6fbe1b3024059f927dd1eee921c4a023ed53e
taylorbyks <taylorbyks@hotmail.com>
2021-08-27 14:56:56 -0300

    Fix style

--- src/screens/WaterTank/styles.js
+++ src/screens/WaterTank/styles.js
@@ -4,6 +4,15 @@ export const WaterTankContainer = styled.View`
   ${({ theme }) => css`
     flex: 1;
     align-items: flex-start;
+    justify-content: space-around;
+    padding: ${theme.metrics.px(40)}px ${theme.metrics.px(15)}px ${theme.metrics.px(48)}px;
+  `}
+`
+
+export const WaterTankFormContainer = styled.View`
+  ${({ theme }) => css`
+    width: 100%;
+    align-items: flex-start;
     justify-content: flex-start;
     padding: ${theme.metrics.px(40)}px ${theme.metrics.px(15)}px ${theme.metrics.px(48)}px;
   `}
